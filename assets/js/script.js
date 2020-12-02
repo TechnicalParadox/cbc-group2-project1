6672dff5545756aee5bb4ebb9e9dee4ba6a6552b
Giamo Lao <giamolao98@gmail.com>
2020-12-01 21:00:32 -0500

    Site automatically loads last search if available

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,24 @@
-// TODO: Pull most recent search automatically from localStorage on load
+/** Loads the most recent search from localStorage and changes #input_search 's
+placeholder value to the most recent search */
+function loadRecent()
+{
+  // Get most recent search from localStorage
+  let storage = window.localStorage; // Reference localStorage
+  let lastSearch = storage.getItem("lastSearch");
 
-/** Handles click of search button. Saves most recent search to localStorage and fetches relevant data from API */
+  // Verify a recent search exists
+  if (lastSearch === null) // If no recent search is saved lastRecent === null is true
+    return; // Return early, no need to go through rest of function
+
+  // Set #input_search placeholder to lastSearch.
+  $("#input_search").attr("placeholder", lastSearch);
+
+  // TODO: Automatically fetch data using lastSearch and populate screen with info
+}
+loadRecent(); // Load most recent search immediately upon startup
+
+/** Handles click of search button. Saves most recent search to localStorage and
+ fetches relevant data from API */
 $("#button_search").click(function()
 {
   // Get input value from #input_search
