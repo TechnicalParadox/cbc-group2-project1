350b74d688793373a24d4bb0a6736c124694dc8a
Giamo Lao <giamolao98@gmail.com>
2020-12-02 03:28:53 -0500

    added API urls and set up the geocoding functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,49 @@
 /** Sunset Sunrise API url example https://sunrise-sunset.org/api
-  * leaving out date defaults to current
-  * https://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400&date=2020-12-02
-  */
- let SS_API_URL = "https://api.sunrise-sunset.org/json?";
+* leaving out date defaults to current, no API key needed
+* https://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400&date=2020-12-02
+*/
+const SS_API_URL = "https://api.sunrise-sunset.org/json?";
 
- /** Attempt to get users location with HTML5 */
- if (navigator.geolocation)
- {
-   navigator.geolocation.getCurrentPosition(loadRecent); // Call loadRecent, passing location
- }
+/** OpenCageData API url example https://opencagedata.com/api
+* Reverse geocoding
+* https://api.opencagedata.com/geocode/v1/json?q=LAT+LNG&key=YOUR-API-KEY
+* Forward geocoding
+* https://api.opencagedata.com/geocode/v1/json?q=PLACENAME&key=YOUR-API-KEY
+*/
+const OCD_GEOC_URL = "https://api.opencagedata.com/geocode/v1/json?";
+const OCD_API_KEY = "1758919effee418b9b3316a3343b24df";
 
-/** Loads the most recent search from localStorage and changes #input_search 's
-placeholder value to the most recent search */
+/** Attempt to get users location with HTML5 */
+if (navigator.geolocation)
+{
+  navigator.geolocation.getCurrentPosition(loadRecent); // Call loadRecent, passing location
+}
+
+function coordsToCity(lat, long)
+{
+  let url = OCD_GEOC_URL + "q=" + lat + "+" + long + "&key=" + OCD_API_KEY;
+  $.get(url, function(response)
+  {
+    // TODO: return city from response
+  });
+}
+
+function cityToCoords(city)
+{
+  let url = OCD_GEOC_URL + "q=" + city + "&key" + OCD_API_KEY;
+  $.get(url, function(response)
+  {
+    // TODO: return coords from response
+  });
+}
+
+/**
+* Loads most recent search or attempts to pull user location, changes
+* #input_search's placeholder to be relevant, calls update to data fetch
+* function while passing coords.
+* @param  {[type]} currentPos - [optional] the users current location, to be
+*                              if no previous search is saved.
+*/
 function loadRecent(currentPos)
 {
   // Get most recent search from localStorage
@@ -22,24 +54,30 @@ function loadRecent(currentPos)
   if (lastSearch === null) // If no recent search is saved
   {
     if (currentPos === undefined) // If no access to users current location
-      return; // Return early, no need to go through rest of function
+    return; // Return early, no need to go through rest of function
 
-    // Get users coordinates from location
+    // Get users coordinates from location and pass to update times
     let lat = currentPos.coords.latitude, long = currentPos.coords.longitude;
+    updateTimes(lat, long);
 
-    // TODO: fetch data using coordinates and populate screen with info
     return; // Return early, no need to go through rest of function
   }
 
   // Set #input_search placeholder to lastSearch.
   $("#input_search").attr("placeholder", lastSearch);
 
-  // TODO: Automatically fetch data using lastSearch and populate screen with info
+  let coords = cityToCoords(lastSearch);
+  updateTimes(coords[0], coords[1]);
 }
 loadRecent(); // Load most recent search immediately upon load
 
+function updateTimes(lat, long)
+{
+
+}
+
 /** Handles click of search button. Saves most recent search to localStorage and
- fetches relevant data from API */
+fetches relevant data from API */
 $("#button_search").click(function()
 {
   // Get input value from #input_search
