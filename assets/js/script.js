12902c109efa54f0708abeb3be46b07b0aecd05a
Giamo Lao <giamolao98@gmail.com>
2020-12-01 20:42:50 -0500

    Most recent search saves to localStorage, added todos

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,16 @@
+// TODO: Pull most recent search automatically from localStorage on load
+
+/** Handles click of search button. Saves most recent search to localStorage and fetches relevant data from API */
 $("#button_search").click(function()
 {
-  console.log($("#input_search").val());
+  // Get input value from #input_search
+  let input = $("#input_search").val();
+
+  // Save most recent search to localStorage
+  let storage = window.localStorage;
+  storage.setItem("lastSearch", input) // Input is saved under the key 'lastSearch'
+
+  console.log(input); // Log value of search input to console.
+
+  // TODO: fetch sunrise/sunset data from OpenWeatherAPI
 });
